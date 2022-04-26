955125b6c242805284cfc2f71fdf55e72d320c8e
Movsisyan <movsisyan@protonmail.com>
2022-04-26 10:03:54 +0400

    Implemented #1, #27, #28 (#30)
    * Implemented #1
    
    * Implemented #27
    
    * Implemented #28

--- entities/ddl.sql
+++ entities/ddl.sql
@@ -1,13 +1,47 @@
-drop table if exists ticket;
+drop table if exists ticket user_, profile_, interval_;
 
 -- Tickets entity:
--- https://github.com/MovsisyanM/DB-Queues/issues/2
+-- (#2) https://github.com/MovsisyanM/DB-Queues/issues/2
 
-create table ticket (
-    id int primary key,
-    created_date timestamp default current_timestamp not null,
-    target_day timestamp not null,
-    check_in boolean not null default false,
-    last_change_date timestamp default current_timestamp,
-    activated_time timestamp
+create table ticket_ (
+    id serial primary key,
+    created_date_ timestamp default current_timestamp not null,
+    target_day_ timestamp not null,
+    check_in_ boolean not null default false,
+    last_change_date_ timestamp default current_timestamp,
+    activated_time_ timestamp
 );
+
+
+-- User entity:
+-- (#1) https://github.com/MovsisyanM/DB-Queues/issues/1
+
+create table user_ (
+    id serial primary key,
+    first_name_ varchar(100) not null,
+    last_name_ varchar(100) not null,
+    email_ varchar(100) not null,
+    joined_since_ timestamp default current_timestamp
+);
+
+-- Profile entity:
+-- (#27) https://github.com/MovsisyanM/DB-Queues/issues/27
+
+create table profile_ (
+    id serial primary key,
+    location_ varchar(50),
+    timezone_ varchar(50),
+    language_ varchar(20),
+    user_id_ int references user_(id)
+);
+
+-- Interval entity:
+-- (#28) https://github.com/MovsisyanM/DB-Queues/issues/28
+
+create table interval_ (
+    id serial primary key,
+    weekday_ varchar(9),
+    start_hour_ timestamp,
+    end_hour_ timestamp
+);
+
