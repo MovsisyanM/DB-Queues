a05558694433f56cc2dc1a78f70e9838fcc10310
MovsisyanN <movsisyan@protonmail.com>
2022-04-26 09:29:34 +0400

    Implemented #1

--- entities/ddl.sql
+++ entities/ddl.sql
@@ -4,10 +4,25 @@ drop table if exists ticket;
 -- https://github.com/MovsisyanM/DB-Queues/issues/2
 
 create table ticket (
-    id int primary key,
+    id serial primary key,
     created_date timestamp default current_timestamp not null,
     target_day timestamp not null,
     check_in boolean not null default false,
     last_change_date timestamp default current_timestamp,
     activated_time timestamp
 );
+
+
+drop table if exists user_;
+
+-- User entity:
+-- https://github.com/MovsisyanM/DB-Queues/issues/1
+
+create table user_ (
+    id serial primary key,
+    first_name varchar(100) not null,
+    last_name varchar(100) not null,
+    email varchar(100) not null,
+    joined_since timestamp default current_timestamp
+)
+
