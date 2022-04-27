f50ab5cb744f8b51ef41d8648ee854df5b3e6d7c
MovsisyanM <movsisyan@protonmail.com>
2022-04-27 21:24:28 +0400

    Data fill - User

---
+++ entities/data_fill.sql
@@ -0,0 +1,22 @@
+-- Inserting data into #1 User entity:
+
+insert into user_ (first_name_, last_name_, email_) 
+values ('Bob', 'Builderian', 'builderian@queues.db'),
+    ('John', 'Doe', 'doe@queues.db'),
+    ('Bill', 'Fisher', 'fisher@queues.db'),
+    ('Papa', 'Juan', 'juan@queues.db'),
+    ('Senior', 'Pepper', 'pepper@queues.db'),
+    ('Marlin', 'Manrole', 'manrole@queues.db'),
+    ('Hose', 'Ignasias', 'ignasias@queues.db'),
+    ('Bubert', 'Scarecroyan', 'scarecroyan@queues.db'),
+    ('Alice', 'Wonderyan', 'wonderyan@queues.db'),
+    ('Addonis', 'Chadyan', 'chadyan@queues.db'),
+    ('Guiseppe', 'Verdi', 'verdi@queues.db'),
+    ('Student', 'T', 't@queues.db'),
+    ('Alberto', 'Einschwein', 'einschwein@queues.db'),
+    ('Molly', 'Popyan', 'popyan@queues.db'),
+    ('Aziz', 'Shavershian', 'zyzz@queues.db'),
+    ('Vahram', 'Papazyan', 'papazyan@queues.db'),
+    ('Bill', 'Nye', 'nye@queues.db'),
+    ('Pepe', 'Frogyan', 'frogyan@queues.db'),
+    ('Doge', 'Coin', 'coin@queues.db');
