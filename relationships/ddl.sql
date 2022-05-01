c1472f764d1edc6696ff208b78a18c91075e2348
MovsisyanM <movsisyan@protonmail.com>
2022-05-01 17:12:25 +0400

    Merged #17, #18

--- relationships/ddl.sql
+++ relationships/ddl.sql
@@ -18,12 +18,13 @@ create table user_book_ticket_ (
 );
 
 
--- employee_works_schedule relationship:
--- (#17)
+-- employee_works_schedule and employee_is_in_branch relationship:
+-- (#17, #18)
 
-create table employee_works_schedule_ (
+create table employee_works_at_ (
     employee_id_ int references employee_(id),
     schedule_id_ int references schedule_(id),
+    branch_id_ int references branch_(id),
     primary key (employee_id_, schedule_id_)
 );
 
