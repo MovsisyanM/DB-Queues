25f9a894dc133570af9db737afee59d21201a28e
MovsisyanM <movsisyan@protonmail.com>
2022-05-01 17:09:38 +0400

    Implemented #17

--- relationships/ddl.sql
+++ relationships/ddl.sql
@@ -1,4 +1,4 @@
-drop table if exists final_status_;
+drop table if exists final_status_, user_book_ticket_, employee_works_schedule_;
 
 -- final_status relationship:
 -- (#12)
@@ -8,6 +8,7 @@ create table final_status_ (
     ticket_log_id_ int references ticket_log_(id)
 );
 
+
 -- user_book_ticket relationship:
 -- (#13)
 
@@ -15,3 +16,14 @@ create table user_book_ticket_ (
     ticket_id_ int references ticket_(id) primary key,
     user_id_ int references user_(id)
 );
+
+
+-- employee_works_schedule relationship:
+-- (#17)
+
+create table employee_works_schedule_ (
+    employee_id_ int references employee_(id),
+    schedule_id_ int references schedule_(id),
+    primary key (employee_id_, schedule_id_)
+);
+
