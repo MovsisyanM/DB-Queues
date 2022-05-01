b81d4a1c8d59f669505b269ca175e0ee80ad6145
MovsisyanM <movsisyan@protonmail.com>
2022-05-01 17:04:06 +0400

    Implemented #13

--- relationships/ddl.sql
+++ relationships/ddl.sql
@@ -8,4 +8,10 @@ create table final_status_ (
     ticket_log_id_ int references ticket_log_(id)
 );
 
--- 
+-- user_book_ticket relationship:
+-- (#13)
+
+create table user_book_ticket_ (
+    ticket_id_ int references ticket_(id) primary key,
+    user_id_ int references user_(id)
+);
