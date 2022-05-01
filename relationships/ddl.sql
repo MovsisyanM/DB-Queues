50d9155884aa78f10494d02cd28a880f0a34d5f6
MovsisyanM <movsisyan@protonmail.com>
2022-05-01 17:42:35 +0400

    Implemented #25

--- relationships/ddl.sql
+++ relationships/ddl.sql
@@ -1,4 +1,10 @@
-drop table if exists branch_open_interval_, final_status_, user_book_ticket_, employee_works_at_;
+drop table if exists ticket_located_in_branch_, 
+    company_provides_service_type_, 
+    company_specializes_in_field_, 
+    branch_open_interval_, 
+    final_status_, 
+    user_book_ticket_, 
+    employee_works_at_;
 
 -- final_status relationship:
 -- (#12)
@@ -56,4 +62,13 @@ create table company_provides_service_type_ (
     service_type_id_ int references service_type_(id),
     primary key (company_id_, service_type_id_)
 );
-)
+
+
+-- company_specializes_in_field 
+-- (#25)
+
+create table company_specializes_in_field_ (
+    company_id_ int references company_(id),
+    field_id_ int references field_(id),
+    primary key (company_id_, field_id_)
+);
