2e3f50d337d4d9696ae0e72647d92c4dae6cf6b1
MovsisyanN <movsisyan@protonmail.com>
2022-05-11 12:26:13 +0400

    #58

--- views/ddl.sql
+++ views/ddl.sql
@@ -23,5 +23,22 @@ create or replace view Most_visited_branches as (
 select * from Most_visited_branches;
 
 
+drop view if exists Ticket_miss_rate_by_user;
 
+create or replace view Ticket_miss_rate_by_user as (
+    select branch_id_ as branch, 
+        count(ticket_id_) as visits 
+    from ticket_located_in_branch_
+    group by branch_id_
+    order by visits desc
+);
+
+select * from Ticket_miss_rate_by_user;
+
+
+select user_id_, sum(case when check_in_ then 1 else 0 end) as check_ins
+from ticket_ 
+join user_book_ticket_ 
+on user_book_ticket_.ticket_id_ = ticket_.id
+group by user_id_;
 
