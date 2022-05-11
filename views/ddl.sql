91a800510ac8b0b19c41921aefa417ddaaead5a4
MovsisyanN <movsisyan@protonmail.com>
2022-05-11 12:13:31 +0400

    #59

--- views/ddl.sql
+++ views/ddl.sql
@@ -8,3 +8,20 @@ create or replace view Most_used_languages as (
 );
 
 select * from Most_used_languages;
+
+
+drop view if exists Most_visited_branches;
+
+create or replace view Most_visited_branches as (
+    select branch_id_ as branch, 
+        count(ticket_id_) as visits 
+    from ticket_located_in_branch_
+    group by branch_id_
+    order by visits desc
+);
+
+select * from Most_visited_branches;
+
+
+
+
