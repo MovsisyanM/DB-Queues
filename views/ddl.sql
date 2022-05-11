00dacb42247d8ff863918368adee80b8553e9777
MovsisyanM <movsisyan@protonmail.com>
2022-05-11 19:29:49 +0400

    #57

--- views/ddl.sql
+++ views/ddl.sql
@@ -36,9 +36,29 @@ create or replace view Ticket_miss_rate_by_user as (
 select * from Ticket_miss_rate_by_user;
 
 
-select user_id_, sum(case when check_in_ then 1 else 0 end) as check_ins
-from ticket_ 
-join user_book_ticket_ 
-on user_book_ticket_.ticket_id_ = ticket_.id
-group by user_id_;
+drop view if exists User_check_in_rate;
+
+
+create or replace view User_check_in_rate as (
+    select user_id_, sum(case when check_in_ then 1 else 0 end) as check_ins
+    from ticket_ 
+    join user_book_ticket_ 
+    on user_book_ticket_.ticket_id_ = ticket_.id
+    group by user_id_
+);
+
+select * from User_check_in_rate;
+
+drop view if exists User_avg_late;
+
+create or replace view User_avg_late as (
+    select user_id_, 
+        avg(activated_time_ - target_day_) as diff
+    from user_book_ticket_ u
+    join ticket_ t on t.id = u.ticket_id_
+    where check_in_ = true
+    group by user_id_
+);
+
+select * from User_avg_late;
 
